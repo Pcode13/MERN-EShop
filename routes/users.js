2798f549cfa8c85f299de822dafc23b5aeca20dc
Pragati Veer <pragati.veer@cloverinfotech.com>
2025-09-09 19:24:27 +0530

    Get User and List of Users Excluding Password

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,45 @@
-const {User} = require('../models/user');
-const express = require('express');
+const { User } = require("../models/user");
+const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 
-router.get(`/`, async (req, res) =>{
-    const userList = await User.find();
+router.get(`/`, async (req, res) => {
+  const userList = await User.find().select("-passwordHash");
 
-    if(!userList) {
-        res.status(500).json({success: false})
-    } 
-    res.send(userList);
-})
+  if (!userList) {
+    res.status(500).json({ success: false });
+  }
+  res.send(userList);
+});
 
-module.exports =router;
+router.get(`/:id`, async (req, res) => {
+  const userList = await User.find()
+    .select("-passwordHash")
+    .findById(req.params.id);
+
+  if (!userList) {
+    res.status(500).json({ success: false });
+  }
+  res.send(userList);
+});
+
+router.post(`/`, async (req, res) => {
+  const userList = new User({
+    name: req.body.name,
+    email: req.body.email,
+    passwordHash: bcrypt.hashSync(req.body.password, 10),
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+    street: req.body.street,
+    apartment: req.body.apartment,
+    zip: req.body.zip,
+    city: req.body.city,
+    country: req.body.country,
+  });
+  const user = await userList.save();
+
+  if (!user) return res.status(500).send("The user cannot be created");
+
+  res.send(user);
+});
+module.exports = router;
